fe9c5cf4a681107cb6171316b7f58244fc7c3273
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-11-24 05:21:47 -0500

    add airport routes as lines

--- mapping_single_point/static/js/logic.js
+++ mapping_single_point/static/js/logic.js
@@ -2,15 +2,15 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
-    center: [34.0522, -118.24],
-    zoom: 4
+    center: [37.6214, -122.3790],
+    zoom: 5
 });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v11',
+    id: 'mapbox/satellite-v9',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
@@ -41,3 +41,15 @@ cityData.forEach(function(city) {
     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
     .addTo(map);
 });
+
+let line = [
+    [33.9416, -118.4085],
+    [37.6214, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow"
+  }).addTo(map);
